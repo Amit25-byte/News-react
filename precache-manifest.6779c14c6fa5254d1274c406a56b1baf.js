9d10b0189444d70dfa950e430e1b2c5712a3f648
Amit Singh <amit.191608@ncit.edu.np>
2023-01-04 20:10:15 +0545

    Updates

---
+++ precache-manifest.6779c14c6fa5254d1274c406a56b1baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eedaafe8a7c1cfb80f78",
+    "url": "/News-react/static/css/main.ce67483a.chunk.css"
+  },
+  {
+    "revision": "eedaafe8a7c1cfb80f78",
+    "url": "/News-react/static/js/main.eedaafe8.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/News-react/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "6296154c48a28afc7245",
+    "url": "/News-react/static/js/2.6296154c.chunk.js"
+  },
+  {
+    "revision": "2aa4d741518e17b17ad3",
+    "url": "/News-react/static/js/runtime~main.2aa4d741.js"
+  },
+  {
+    "revision": "8694cc6b612abd165d1b3854450d759a",
+    "url": "/News-react/index.html"
+  }
+];
